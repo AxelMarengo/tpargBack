237ac0fa8a0d8a752420eeb7ff78140e49ef1c29
axel <axelmarengo@gmail.com>
2023-05-10 18:31:45 -0300

    cambio cors

--- src/main/java/com/example/demo/Controller/CSkill.java
+++ src/main/java/com/example/demo/Controller/CSkill.java
@@ -18,7 +18,7 @@ import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@CrossOrigin(origins = "https://argprograma-f2d81.firebaseapp.com/")
+@CrossOrigin(origins = "https://argprograma-f2d81.firebaseapp.com")
 
 public class CSkill {
     
